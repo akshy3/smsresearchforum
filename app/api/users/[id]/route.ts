adec90c885067e48d8ecf4cf6a0a16cf82cb0e16
akshy3 <akshayashok209@gmail.com>
2026-02-06 01:02:42 +0530

    update

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,28 +1,33 @@
 import { prisma } from '@/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+
   const user = await prisma.user.findUnique({
-    where: { id: Number(context.params.id) },
+    where: { id: Number(id) },
   })
 
   return NextResponse.json(user)
 }
 
-export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+export async function PUT(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
   const body = await req.json()
 
   const user = await prisma.user.update({
-    where: { id: Number(context.params.id) },
+    where: { id: Number(id) },
     data: body,
   })
 
   return NextResponse.json(user)
 }
 
-export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+
   await prisma.user.delete({
-    where: { id: Number(context.params.id) },
+    where: { id: Number(id) },
   })
 
   return NextResponse.json({ success: true })
