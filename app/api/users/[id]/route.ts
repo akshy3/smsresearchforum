3c049b72b91366eaf7b31279bb9f984b31d66ea7
akshy3 <akshayashok209@gmail.com>
2026-02-06 00:59:40 +0530

    update

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,28 +1,28 @@
 import { prisma } from '@/lib/prisma'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, context: { params: { id: string } }) {
   const user = await prisma.user.findUnique({
-    where: { id: Number(params.id) },
+    where: { id: Number(context.params.id) },
   })
 
   return NextResponse.json(user)
 }
 
-export async function PUT(req: Request, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
   const body = await req.json()
 
   const user = await prisma.user.update({
-    where: { id: Number(params.id) },
+    where: { id: Number(context.params.id) },
     data: body,
   })
 
   return NextResponse.json(user)
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
   await prisma.user.delete({
-    where: { id: Number(params.id) },
+    where: { id: Number(context.params.id) },
   })
 
   return NextResponse.json({ success: true })
