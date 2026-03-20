99f24ef49dad88bba4221f2cb0067b2c74103a4b
akshy3 <akshayashok209@gmail.com>
2026-03-20 16:52:44 +0530

    strategy track rename

--- data/conferenceData.ts
+++ data/conferenceData.ts
@@ -96,7 +96,7 @@ export const conferenceTracks = [
   },
   {
     slug: 'strategy',
-    title: 'Strategy',
+    title: 'Strategy & Innovation',
     subtitle: 'Innovation and Emerging Business Models in the Digital Economy',
     description:
       'This track looks at how firms rethink innovation, competition, and value creation in response to technological change and digital disruption.',
